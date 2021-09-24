23a0f834051f0e512601bcfc4c3c07797932ad5b
OhMinSeop <mins5190@gmail.com>
2021-09-25 02:02:14 +0900

    ADD: 메인 페이지 구성

---
+++ src/components/common/NFTCard.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { MockIllustrationItem } from '@api/local/get-mock-illustractions';
+
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+interface NFTCardProps {
+  item: MockIllustrationItem;
+}
+
+const NFTCard: React.FC<NFTCardProps> = ({ item }) => {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar
+            sx={{ bgcolor: red[500] }}
+            src={item.profileImageUrl}
+            aria-label="recipe"
+          >
+            {item.nickname}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={item.name}
+        subheader={`CreateBy ${item.nickname}`}
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={item.image}
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary" noWrap>
+          {item.description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>{item.description}</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+};
+
+export default NFTCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,39 +2,123 @@ import React from 'react';
 
 // components
 import Box from '@mui/material/Box';
-import Masonry from '@mui/lab/Masonry';
-import MasonryItem from '@mui/lab/MasonryItem';
-import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { grey } from '@mui/material/colors';
 
 import AppLayout from '@components/layouts/AppLayout';
+import NFTCard from '@components/common/NFTCard';
+
 import { useIllustractionsQuery } from '@api/local/get-mock-illustractions';
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 const IndexPage = () => {
   const result = useIllustractionsQuery();
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
   const items = result.data?.items ?? [];
 
   console.log('items', items);
 
   return (
-    <Container maxWidth="xl">
-      <Box p={3}>
-        <Masonry
-          columns={{ xs: 1, sm: 2, md: 3, lg: 4 }}
-          sx={{ height: '100%' }}
-          spacing={2}
-        >
-          {items.map((item, i) => {
-            console.log(i, item);
-            return (
-              <MasonryItem key={item.tokenId}>
-                <img src={item.image} alt={item.name} loading="lazy" />
-              </MasonryItem>
-            );
-          })}
-        </Masonry>
+    <div>
+      <Box sx={{ flexGrow: 1, bgcolor: grey[50] }}>
+        <Box sx={{ width: '100%' }} className="space-y-5 pt-8">
+          <Box sx={{ borderBottom: 1, borderColor: 'transparent' }}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              textColor="inherit"
+              indicatorColor="secondary"
+              centered
+              aria-label="basic tabs example"
+            >
+              <Tab label="최신순" {...a11yProps(0)} />
+              <Tab label="트렌딩" {...a11yProps(1)} />
+              <Tab label="아무개" {...a11yProps(2)} />
+            </Tabs>
+          </Box>
+          <TabPanel value={value} index={0}>
+            <Grid container spacing={3} direction="row" justifyContent="center">
+              {items.map((item) => (
+                <Grid
+                  item
+                  columns={{ xl: 4, xs: 4, lg: 3, md: 3, sm: 5 }}
+                  key={`nft-${item.tokenId}`}
+                >
+                  <NFTCard item={item} />
+                </Grid>
+              ))}
+            </Grid>
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <Grid container spacing={3} direction="row" justifyContent="center">
+              {items.map((item) => (
+                <Grid
+                  item
+                  columns={{ xl: 4, xs: 4, lg: 3, md: 3, sm: 5 }}
+                  key={`nft-${item.tokenId}`}
+                >
+                  <NFTCard item={item} />
+                </Grid>
+              ))}
+            </Grid>
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            <Grid container spacing={3} direction="row" justifyContent="center">
+              {items.map((item) => (
+                <Grid
+                  item
+                  columns={{ xl: 4, xs: 4, lg: 3, md: 3, sm: 5 }}
+                  key={`nft-${item.tokenId}`}
+                >
+                  <NFTCard item={item} />
+                </Grid>
+              ))}
+            </Grid>
+          </TabPanel>
+        </Box>
       </Box>
-    </Container>
+    </div>
   );
 };
 
