2bd546c776c02214e7eabeeb7efd457cf3c1acfc
OhMinSeop <mins5190@gmail.com>
2021-11-07 22:22:25 +0900

    MOD: 수정

--- src/components/story/detail/StickyHistoryTable.tsx
+++ src/components/story/detail/StickyHistoryTable.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Paper from '@mui/material/Paper';
+
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -55,23 +55,7 @@ function createData(
   return { name, code, population, size, density };
 }
 
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
+const rows = [createData('India', 'IN', 1324171354, 3287263)];
 
 interface StickyHistoryTableProps {
   loading: boolean;
